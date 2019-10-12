09a721751e62917cef374e10257fde06724aba21
James G Garcia <jimmygus22@gmail.com>
2019-10-12 14:14:47 -0500

    Update organism.cpp

--- src/organism.cpp
+++ src/organism.cpp
@@ -8,23 +8,24 @@ class Organism {
     public:
         int x_pos;
         int y_pos;
-        int surfaceHeight;
-        int surfaceWidth;
-        char gene;
-        char start;
         void create_organism(int height, int width);
         void move();
     private:
+        int surfaceHeight;
+        int surfaceWidth;
+        char start;
         void start_position();
         void primary_direction();
 } ;
 
+//public function to create an orgnaism
 void Organism::create_organism(int height, int width) {
     surfaceHeight = height;
     surfaceWidth = width;
     start_position();
 }
 
+//Randomly selects start position along edge of surface
 void Organism::start_position() {
     switch (rand() % 4)
     {
@@ -54,60 +55,77 @@ void Organism::start_position() {
     }
 }
 
+//Moves organism. If on wall and moves toward wall will move opposite direction
 void Organism::move() {
-    switch (rand() % 6)
+    switch (rand() % 5)
     {
     case 0:
-    case 1:
         primary_direction();
         break;
-    case 2:
-        if (x_pos == surfaceWidth - 1)
+    case 1:
+        if (x_pos == surfaceWidth - 1) {
+            x_pos -= 1;
             break;
+        }
         x_pos += 1;
         break;
-    case 3:
-        if (x_pos == 0)
+    case 2:
+        if (x_pos == 0) {
+            x_pos += 1;
             break;
+        }
         x_pos -= 1;
         break;
-    case 4:
-        if (y_pos == surfaceHeight - 1)
+    case 3:
+        if (y_pos == surfaceHeight - 1) {
+            y_pos -= 1;
             break;
+        }
         y_pos +=1;
         break;
-    case 5:
-        if (y_pos == 0)
+    case 4:
+        if (y_pos == 0) {
+            y_pos += 1;
             break;
+        }
         y_pos -= 1;
         break;
     default:
-        cout << "Error: rand() % 6 value outside expected range";
+        cout << "Error: rand() % 5 value outside expected range";
         break;
     }
 }
 
+//Increased chance in moving towards opposite wall they started L->R, T->B ...
 void Organism::primary_direction() {
     switch (start)
     {
     case 'L':
-        if (x_pos == surfaceWidth - 1)
+        if (x_pos == surfaceWidth - 1) {
+            x_pos -= 1;
             break;
+        }
         x_pos += 1;
         break;
     case 'R':
-        if (x_pos == 0)
+        if (x_pos == 0) {
+            x_pos += 1;
             break;
+        }
         x_pos -= 1;
         break;
     case 'T':
-        if (y_pos == surfaceHeight - 1)
+        if (y_pos == surfaceHeight - 1) {
+            y_pos -= 1;
             break;
+        }
         y_pos += 1;
         break;
     case 'B':
-        if (y_pos == 0)
+        if (y_pos == 0) {
+            y_pos += 1;
             break;
+        }
         y_pos -= 1;
         break;
     default:
