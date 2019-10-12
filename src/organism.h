3da06c48427a367e8096b0975bd8a26813846329
James G Garcia <jimmygus22@gmail.com>
2019-10-12 17:47:36 -0500

    Update organism.h
    added return side

--- src/organism.h
+++ src/organism.h
@@ -6,15 +6,20 @@ class Organism {
     public:
         int x_pos;
         int y_pos;
+        bool nourished = false;
+        bool nour_and_return = false;
         void create_organism();
         void move();
+        void food_check(char** surface);
     private:
         Settings s;
         int surfaceHeight = s.height;
         int surfaceWidth = s.width;
+        char side;
         char start;
         void start_position();
         void primary_direction();
+        void return_side();
 } ;
 
 #endif
