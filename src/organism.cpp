acfdc9694420bff5a234fe9c79a96d831980e2ed
James G Garcia <jimmygus22@gmail.com>
2019-10-12 17:46:21 -0500

    Update organism.cpp
    Added return side

--- src/organism.cpp
+++ src/organism.cpp
@@ -9,6 +9,7 @@ using std::cout;
 //public function to create an orgnaism
 void Organism::create_organism() {
     start_position();
+    return;
 }
 
 //Randomly selects start position along edge of surface
@@ -39,47 +40,90 @@ void Organism::start_position() {
         cout << "Error: rand() % 4 value outside expected range";
         break;
     }
+    return;
 }
 
-//Moves organism. If on wall and moves toward wall will move opposite direction
+/*Moves organism randomly Till organism gets food
+with food organism moves to closest side*/
 void Organism::move() {
-    switch (rand() % 5)
-    {
-    case 0:
-        primary_direction();
-        break;
-    case 1:
-        if (x_pos == surfaceWidth - 1) {
-            x_pos -= 1;
+    if (!nourished && !nour_and_return) {
+        switch (rand() % 5)
+        {
+        case 0:
+            primary_direction();
             break;
-        }
-        x_pos += 1;
-        break;
-    case 2:
-        if (x_pos == 0) {
+        case 1:
+            if (x_pos == surfaceWidth - 1) {
+                x_pos -= 1;
+                break;
+            }
             x_pos += 1;
             break;
-        }
-        x_pos -= 1;
-        break;
-    case 3:
-        if (y_pos == surfaceHeight - 1) {
+        case 2:
+            if (x_pos == 0) {
+                x_pos += 1;
+                break;
+            }
+            x_pos -= 1;
+            break;
+        case 3:
+            if (y_pos == surfaceHeight - 1) {
+                y_pos -= 1;
+                break;
+            }
+            y_pos +=1;
+            break;
+        case 4:
+            if (y_pos == 0) {
+                y_pos += 1;
+                break;
+            }
             y_pos -= 1;
             break;
-        }
-        y_pos +=1;
-        break;
-    case 4:
-        if (y_pos == 0) {
-            y_pos += 1;
+        default:
+            cout << "Error: rand() % 5 value outside expected range";
             break;
         }
-        y_pos -= 1;
-        break;
-    default:
-        cout << "Error: rand() % 5 value outside expected range";
-        break;
     }
+    else if (nourished && !nour_and_return) {
+        switch (side) {
+        case 'L':
+            if (x_pos == 0) {
+                nour_and_return = true;
+                break;
+            }
+            x_pos -= 1;
+            break;
+        case 'R':
+            if (x_pos == surfaceWidth - 1) {
+                nour_and_return = true;
+                break;
+            }
+            x_pos += 1;
+            break;
+        case 'T':
+            if (y_pos == 0) {
+                nour_and_return = true;
+                break;
+            }
+            y_pos -= 1;
+            break;
+        case 'B':
+            if (y_pos == surfaceHeight - 1) {
+                nour_and_return = true;
+                break;
+            }
+            y_pos += 1;
+            break;
+        default:
+            cout << "Error: non-inilizted side chracter";
+            break;
+        }   
+    }
+    else if (nourished && nour_and_return){
+        ;
+    }
+    return;
 }
 
 //Increased chance in moving towards opposite wall they started L->R, T->B ...
@@ -118,4 +162,41 @@ void Organism::primary_direction() {
         cout << "Error: non-inilizted start chracter";
         break;
     }
+    return;
+}
+
+//Checks choosen move spot for food
+void Organism::food_check(char** surface) {
+    if (surface[x_pos][y_pos] == '*') {
+        nourished = true;
+        return_side();
+    }
+    return;
+}
+
+//Chooses shortest return after getting food
+void Organism::return_side() {
+    int shortest_x, shortest_y;
+    char side_x, side_y;
+    if ((surfaceWidth - 1 - x_pos) > (x_pos - 0)) {
+        shortest_x = x_pos;
+        side_x = 'L';
+    }
+    else {
+        shortest_x = surfaceWidth - 1 - x_pos;
+        side_x = 'R';
+    }
+    if ((surfaceHeight - 1 - y_pos) > (y_pos - 0)) {
+        shortest_y = y_pos;
+        side_y = 'T';
+    }
+    else {
+        shortest_y = surfaceHeight - 1 - y_pos;
+        side_y = 'B';
+    }
+    if (shortest_x > shortest_y)
+        side = side_y;
+    else
+        side = side_x;
+    return;
 }
