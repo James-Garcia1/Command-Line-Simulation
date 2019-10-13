d7b6b5db2e3b44a55cb1d4ef82dafb9c1e9e9487
James G Garcia <jimmygus22@gmail.com>
2019-10-13 16:55:51 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,77 +7,11 @@
 #include <map>
 #include "settings.h"
 #include "organism.h"
+#include "manage.h"
 
 using std::cout;
 using std::cin;
 using std::vector;
-
-//Creates a pointer vector to objects of class Organism that are on the heap
-vector<Organism*> instance_organisms(const int numOrganisms, const vector<char> IDlist) {
-    vector <Organism*> organisms;
-    
-    for (unsigned int i = 0; i < numOrganisms; ++i) {
-        organisms.push_back(new Organism);
-        organisms[i]->ID = IDlist[i];
-    }
-
-    return organisms;
-}
-
-//Creates 2D array on heap of the surface that will be outputed
-void create_surface(char **surface, Settings s) {
-    for (unsigned int i = 0; i < s.width; ++i) {
-        surface[i] = new char[s.height];
-        for (unsigned int j = 0; j < s.height; ++j){
-            surface[i][j] = '-';
-        }
-    }
-}
-
-//Clears surface 2D array
-void clear_surface(char **surface, Settings s) {
-    for (unsigned int i = 0; i < s.width; ++i) {
-        for (unsigned int j = 0; j < s.height; ++j){
-            surface[i][j] = '-';
-        }
-    }
-}
-
-//Determine if space starts with food based of frequency
-bool rand_food(Settings s) {
-    int chance = rand() % 100;
-    if (chance < s.foodFrequency){
-        return true;
-    }
-    return false;
-}
-
-//Creates Food on surface
-void create_food(char **surface, Settings s) {
-    int food_height = s.height - 1;
-    int food_width = s.width - 1;
-
-    for(unsigned int i = 1; i < food_width; ++i) {
-        for (unsigned int j = 1; j < food_height; ++j) {
-            if (rand_food(s)) {
-                surface[i][j] = '*';
-            }
-        }
-    }   
-}
-
-//Output after generation
-void generation_output(vector<char> IDlist) {
-    int modeID;
-    std::map<char,int> output_modes;
-    for(auto& x : IDlist) {
-        modeID = std::count(IDlist.begin(), IDlist.end(), x);
-        output_modes[x] = modeID;
-    }
-    for(auto& [key, value]: output_modes) {
-        cout << key << ": " << value << '\n';
-    }
-}
 
 main() {
     //random seed so movement is random
@@ -89,7 +23,7 @@ main() {
     vector<int> old_x_pos;
     vector<int> old_y_pos;
     vector<char> IDlist;
-    char** surface = new char*[s.width];
+    char** surface = new char*[s.height];
     
     create_surface(surface, s);
     
@@ -115,8 +49,8 @@ main() {
         
         for (unsigned int dayCounter = 0; dayCounter < s.numDays; ++dayCounter) {
             //Outputs surface
-            for (int i = 0; i < s.width; ++i) {
-                for (int j = 0; j < s.height; ++j){
+            for (int i = 0; i < s.height; ++i) {
+                for (int j = 0; j < s.width; ++j){
                     cout << surface[i][j];
                 }
                 cout << '\n';
@@ -165,7 +99,7 @@ main() {
     }
 
     //removes from heap
-    for (int i = 0; i < s.width; ++i) {
+    for (int i = 0; i < s.height; ++i) {
         delete surface[i];
     }
     delete surface;
