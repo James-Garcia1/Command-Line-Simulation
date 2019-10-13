261a1a46ce37c3ebd6780a30b38a7fb5bf4d3bcf
James G Garcia <jimmygus22@gmail.com>
2019-10-13 18:57:09 -0500

    Update settings.h

--- src/settings.h
+++ src/settings.h
@@ -2,9 +2,16 @@
 #define SETTINGS_H
 
 struct Settings {
-    const int width = 10;
-    const int height = 7;
-    const int foodFrequency = 30;
+    //vertical length of area
+    const int width = 14;
+    //horizontal length of area
+    const int height = 10;
+    //percentage of food spawn on inner spaces
+    const int foodFrequency = 25;
+    //number of times simulation will reset and run for numDays
+    const unsigned int numGenerations = 100;
+    //number of times simulation will run between generations
+    const unsigned int numDays = 20;
 };
 
 #endif
